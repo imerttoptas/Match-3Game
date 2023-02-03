e603b3790b6de94324210e297bcddca784d73ee0
iMert <toptasi20@itu.edu.tr>
2023-02-03 23:34:15 +0300

    Added MatchChecker
    Made a seperate class for checking matches

---
+++ Match3-Game/Assets/Scripts/Controllers/MatchChecker.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MatchChecker : MonoBehaviour
+{
+    
+    [SerializeField] private GridManager _gridManager;
+
+    public bool IsThereAMatch(Cell cell, BlockType blockType)
+    {
+        Debug.Log(blockType + "for" + cell.name);
+        return GetVerticalMatches(cell, blockType).Count >= 2 || GetHorizontalMatches(cell, blockType).Count >= 2;
+    }
+    
+    public List<Cell> GetAllMatches(Cell cell)
+    {
+        BlockType targetType = cell.GetChildBlock().BlockType;
+        
+        List<Cell> verticalCells = GetMatchingCellsInDirection(cell, new Vector2(1, 0), targetType)
+            .Concat(GetMatchingCellsInDirection(cell, new Vector2(-1, 0), targetType)).ToList();
+        List<Cell> horizontalCells = GetMatchingCellsInDirection(cell, new Vector2(0, 1), targetType)
+            .Concat(GetMatchingCellsInDirection(cell, new Vector2(0, -1), targetType)).ToList();
+
+        return verticalCells.Concat(horizontalCells).ToList();
+    }
+
+     private List<Cell> GetVerticalMatches(Cell cell, BlockType targetType)
+     {
+         List<Cell> verticalCells = GetMatchingCellsInDirection(cell, new Vector2(1, 0), targetType)
+             .Concat(GetMatchingCellsInDirection(cell, new Vector2(-1, 0), targetType)).ToList();
+
+         return verticalCells;
+     }
+     
+     private List<Cell> GetHorizontalMatches(Cell cell, BlockType targetType)
+     {
+         List<Cell> horizontalCells = GetMatchingCellsInDirection(cell, new Vector2(0, 1), targetType)
+             .Concat(GetMatchingCellsInDirection(cell, new Vector2(0, -1), targetType)).ToList();
+
+         return horizontalCells;
+     }
+     
+     /// <summary>
+     /// This function returns list of Cell, that matches with the given cell type in the given direction.
+     /// </summary>
+     /// <param name="cell">Cell to look for matches</param>
+     /// <param name="direction">Direction to look for cells</param>
+     /// <param name="targetBlockType">Block type for Match control</param>
+     /// <param name="maxSteps">Number of cells to look in the given direction</param>
+     /// <returns>the list of matching cells found in the given direction.</returns>
+     private List<Cell> GetMatchingCellsInDirection(Cell cell, Vector2 direction,BlockType targetBlockType,int maxSteps = 2)
+     {
+        List<Cell> matchingCells = new List<Cell>();
+        int rowStep = (int)direction.x;
+        int colStep = (int)direction.y;
+        Cell nextCell = _gridManager.TryGetCellAt(cell.RowNumber + rowStep, cell.ColNumber + colStep);
+        int steps = 0;
+        while (IsCellMatching(nextCell, targetBlockType) && steps < maxSteps)
+        {
+            matchingCells.Add(nextCell);
+            nextCell = _gridManager.TryGetCellAt(nextCell.RowNumber + rowStep, nextCell.ColNumber + colStep);
+            steps++;
+        }
+        return matchingCells;
+     }
+    /// <summary>
+    /// Checks if the the given Cell's BlockType is same with the given targetBlockType 
+    /// </summary>
+    /// <param name="cell">Cell to compare BLockType</param>
+    /// <param name="targetBlockType">target BLockType </param>
+    /// <returns>True if the BlockType of Cell is same with given BlockType, otherwise false </returns>
+    private bool IsCellMatching(Cell cell,BlockType targetBlockType)
+    {
+        return cell && cell.GetChildBlock().BlockType == targetBlockType;
+    }
+    
+}
--- Match3-Game/Assets/Scripts/Managers/GridManager.cs
+++ Match3-Game/Assets/Scripts/Managers/GridManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,12 +8,14 @@ using DG.Tweening;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private GridBuilder gridBuilder;
+    [SerializeField] private MatchChecker matchChecker;
     [SerializeField] private LevelGenerator levelBuilder;
     [SerializeField] private int gridWidth, gridHeight;
     [SerializeField] private float offset;
     private List<Cell> _cellList = new List<Cell>();
     private MatchType _matchType;
-    private System.Action<Cell> OnSwappedCell;
+    // public Action<List<Cell>> OnCellsSwapped;
+
 
     void Start()
     {
@@ -21,91 +24,39 @@ public class GridManager : MonoBehaviour
     }
     
     /// <summary>
-    /// Swaps MatchObjects of two cells
+    /// Swaps Blocks of two cells
     /// </summary>
-    /// <param name="firstCell">first MatchObject's cell</param>
-    /// <param name="secondCell">second MatchObject's cell</param>
-    public void SwapMatchObjects(Cell firstCell, Cell secondCell)
+    /// <param name="firstCell">first Block's cell</param>
+    /// <param name="secondCell">second Block's cell</param>
+    public void SwapBlocks(Cell firstCell, Cell secondCell)
     {
-        Transform tempTransform = firstCell.transform;
-        MatchObject firstMatchObject = firstCell.GetChildMatchObject();
-        MatchObject secondMatchObject = secondCell.GetChildMatchObject();
-
-        firstMatchObject.transform.DOMove(secondCell.transform.position, 0.5f);
-        firstMatchObject.transform.SetParent(secondCell.transform);
-        secondMatchObject.transform.DOMove(tempTransform.position, 0.5f);
-        secondMatchObject.transform.SetParent(tempTransform);
+        Debug.Log("first cell1" + firstCell.name + "second cell1" + secondCell.name);
+        Transform firstCellTransform = firstCell.transform;
+        Transform secondCellTransform = secondCell.transform;
+        Block firstBlock = firstCell.GetChildBlock();
+        Block secondBlock = secondCell.GetChildBlock();
+        
+        secondBlock.transform.SetParent(firstCellTransform);
+        firstBlock.transform.SetParent(secondCellTransform);
+        
+        Debug.Log("is there a match first cell" + matchChecker.IsThereAMatch(firstCell,secondBlock.BlockType));
+        Debug.Log("is there a match second cell" + matchChecker.IsThereAMatch(secondCell,firstBlock.BlockType));
+        if (matchChecker.IsThereAMatch(firstCell,secondBlock.BlockType) || matchChecker.IsThereAMatch(secondCell,firstBlock.BlockType))
+        {
+            firstBlock.transform.DOMove(secondCellTransform.position, 0.5f);
+            secondBlock.transform.DOMove(firstCellTransform.position, 0.5f);
+        }
+        else
+        {
+             firstBlock.transform.DOMove(secondCellTransform.position, 0.5f).OnComplete(()=>firstBlock.transform.DOMove(firstCellTransform.position,0.5f));
+             secondBlock.transform.DOMove(firstCellTransform.position, 0.5f).OnComplete(()=>secondBlock.transform.DOMove(secondCellTransform.position,0.5f));
+             secondBlock.transform.SetParent(secondCellTransform);
+             firstBlock.transform.SetParent(firstCellTransform);
+        }
     }
     
     
-    /// <summary>
-    /// Checks for possible matches for the given cell
-    /// </summary>
-    /// <param name="cell">Cell to check possible matches</param>
-    /// <returns>True if match is found, otherwise false</returns>
-    public bool CheckForMatches(Cell cell)
-    {
-        MatchObjectType targetType = cell.GetChildMatchObject().MatchObjectType;
-        bool isHorizontalMatch = false;
-        bool isVerticalMatch = false;
-        
-        List<Cell> verticalCells = GetMatchingCellsInDirection(cell, new Vector2(1, 0), targetType)
-            .Concat(GetMatchingCellsInDirection(cell, new Vector2(-1, 0), targetType)).ToList();
-        List<Cell> horizontalCells = GetMatchingCellsInDirection(cell, new Vector2(0, 1), targetType)
-            .Concat(GetMatchingCellsInDirection(cell, new Vector2(0, -1), targetType)).ToList();
-        
-        if (verticalCells.Count>=2)
-        { 
-            isHorizontalMatch = true;
-        }
-        if (horizontalCells.Count>=2)
-        {
-            isVerticalMatch = true;
-        }
-
-        if (isHorizontalMatch || isVerticalMatch)
-        {
-            return true;
-        }
-
-        return false;
-    }
-    
-    /// <summary>
-    /// This function returns list of MatchObject, that matches with the given cell type in the given direction.
-    /// </summary>
-    /// <param name="cell">Cell to look for matchs</param>
-    /// <param name="direction">Direction to look for cells</param>
-    /// <param name="targetMatchObjectType">Match object type for Match control</param>
-    /// <param name="maxSteps">Number of cells to look in the given direction</param>
-    /// <returns>the list of matching cells found in the given direction.</returns>
-    private List<Cell> GetMatchingCellsInDirection(Cell cell, Vector2 direction,MatchObjectType targetMatchObjectType,int maxSteps = 2)
-    {
-        List<Cell> matchingCells = new List<Cell>();
-        int rowStep = (int)direction.x;
-        int colStep = (int)direction.y;
-        Cell nextCell = TryGetCellAt(cell.RowNumber + rowStep, cell.ColNumber + colStep);
-        int steps = 0;
-        while (IsMatchingCell(nextCell, targetMatchObjectType) && steps < maxSteps)
-        {
-            matchingCells.Add(nextCell);
-            nextCell = TryGetCellAt(nextCell.RowNumber + rowStep, nextCell.ColNumber + colStep);
-            steps++;
-        }
-        return matchingCells;
-    }
-    /// <summary>
-    /// Checks if the the given Cell's MatchObjectType is same with the given MatchObjectType 
-    /// </summary>
-    /// <param name="cell">Cell to compare MatchObjectType</param>
-    /// <param name="matchObjectType">target MatchObjectType </param>
-    /// <returns>True if the MatchObjectType of Cell is same with given MatchObjectType, otherwise false </returns>
-    private bool IsMatchingCell(Cell cell,MatchObjectType matchObjectType)
-    {
-        return cell && cell.GetChildMatchObject().MatchObjectType == matchObjectType;
-    }
-        
-    private MatchType FindMatchType(List<MatchObject> horizontal, List<MatchObject> vertical)
+    private MatchType FindMatchType(List<Block> horizontal, List<Block> vertical)
     {
         return _matchType;
     }
@@ -142,7 +93,7 @@ public class GridManager : MonoBehaviour
     /// <param name="col">col number of desired cell</param>
     /// <returns>Returns a Cell object if the row and col values are within the grid bounds.
     /// Otherwise, it returns null.</returns>
-    private Cell TryGetCellAt(int row, int col)
+    public Cell TryGetCellAt(int row, int col)
     {
         if (row < 0 || row >=gridWidth || col < 0 || col >= gridHeight)
         {
@@ -151,5 +102,6 @@ public class GridManager : MonoBehaviour
         
         return _cellList[row*gridWidth + col];;
     }
+    
 }
 
